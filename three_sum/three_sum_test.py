7002b0278178f3743d332d1c76137af0d5f447e3
Aleksey Potapov <potalestor@gmail.com>
2020-05-15 00:16:36 +0300

    test updated

--- three_sum/three_sum_test.py
+++ three_sum/three_sum_test.py
@@ -6,11 +6,10 @@ class TestSolution(unittest.TestCase):
     def setUp(self):
         self.solution = Solution()
     def test_1(self):
-        self.assertEqual(self.solution.threeSum([-5, 0, 1, 2, 3, 4, 5]), [[-5, 0, 5]])    
+        self.assertEqual(self.solution.threeSum([-5, 0, 1, 2, 3, 4, 5]), [(-5, 0, 5), (-5, 1, 4), (-5, 2, 3)])    
 
-
-    # def test_2(self):
-    #     self.assertEqual(self.solution.threeSum([-1, 0, 1, 2, -1, -4]), [[-1, 0, 1], [-1, -1, 2]])
+    def test_2(self):
+        self.assertEqual(self.solution.threeSum([-1, 0, 1, 2, -1, -4]), [(-1, -1, 2), (-1, 0, 1)])
 
 
 
