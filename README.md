a534c08acb137f2d2feaf470e14f4e4321755cb3
Aleksey Potapov <potalestor@gmail.com>
2020-05-16 20:44:26 +0300

    Updated cases

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # py-cases
 
 [Two sum](./two_sum/solution.py)
+
 [Three sum](./three_sum/solution.py)
+
 [Add two numbers](./add_two_numbers/solution.py)
--- three_sum/solution.py
+++ three_sum/solution.py
@@ -1,7 +1,27 @@
 from typing import List
 import bisect
 
-# https://leetcode.com/problems/3sum/
+'''
+https://leetcode.com/problems/3sum
+
+Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? 
+Find all unique triplets in the array which gives the sum of zero.
+
+Note:
+
+The solution set must not contain duplicate triplets.
+
+Example:
+
+Given array nums = [-1, 0, 1, 2, -1, -4],
+
+A solution set is:
+[
+  [-1, 0, 1],
+  [-1, -1, 2]
+]
+
+'''
 class Solution:
     # Runtime: 196 ms, faster than 99.92% of Python3 online submissions for 3Sum.
     def threeSum(self, nums: List[int]) -> List[List[int]]:
